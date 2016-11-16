ecce09b93d37f9597620a8f1633362cf18e4f2e5
momothefirst <ricardo.barrote@gmail.com>
2016-11-16 20:40:53 +0000

    navbar
    Inseri navbar lateral, primeira iteraçao

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>Ricardo Barrote - Disruptive Designer</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/main.css">  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+    
+<body>
+    <div id="menubar">
+            <nobr><a href="#"><span class="glyphicon glyphicon-user"></span> Acerca</a></nobr>
+            <nobr><a href="#">Produtos</a></nobr>
+            <nobr><a href="#">Trabalhos</a></nobr>
+            <nobr><a href="#">Contactos</a></nobr>
+    </div>
+    
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(function () {
+
+    $("#menubar").hover(
+        function () {
+            $("#menubar").width(300);
+        },
+        function () {
+            $("#menubar").width(50);
+        }
+    );
+    
+    
+    
+    
+});
