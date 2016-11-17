a9578d04d342c673882a70626142f6f4c0338ffb
momothefirst <ricardo.barrote@gmail.com>
2016-11-17 17:19:13 +0000

    navbar external file

---
+++ navbar.php
@@ -0,0 +1,6 @@
+<div id="menubar">
+            <nobr><a href="#"><span class="glyphicon glyphicon-user"></span>Acerca</a></nobr>
+            <nobr><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>Produtos</a></nobr>
+            <nobr><a href="#"><span class="glyphicon glyphicon-folder-open"></span>Trabalhos</a></nobr>
+            <nobr><a href="#"><span class="glyphicon glyphicon-list-alt"></span>Contactos</a></nobr>
+    </div>
